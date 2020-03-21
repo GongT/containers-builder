4fc99e774c8b44d9c0d127439a77c3934f10400b
GongT <admin@gongt.me>
2020-03-21 15:55:48 +0800

    add wait pid

--- tools/service-wait.sh
+++ tools/service-wait.sh
@@ -3,11 +3,27 @@
 set -Eeuo pipefail
 ARGS=("$@")
 
+PIDFile=/run/$CONTAINER_ID.conmon.pid
+
+function debug() {
+	echo "[wait-run] $*" >&2
+}
+
 function __run() {
-	echo podman "${ARGS[@]}"
+	debug " + podman ${ARGS[*]}"
 	podman "${ARGS[@]}" &
-	PIDFile=/run/$CONTAINER_ID.conmon.pid
-	echo "Conmon PID: $(<$PIDFile)"
+	local I=10
+	while [[ $I -gt 0 ]]; do
+		I=$(($I - 1))
+		if [[ -e "$PIDFile" ]]; then
+			debug "Conmon PID: $(<$PIDFile)"
+			return
+		fi
+		debug "   wait pid $I/10"
+		sleep 1
+	done
+
+	die "Fatal: podman not create pid file: $PIDFile"
 }
 function sdnotify() {
 	if [[ "${NOTIFY_SOCKET+found}" = found ]]; then
@@ -15,50 +31,49 @@ function sdnotify() {
 	fi
 }
 
-echo "[wait-run] Wait container $CONTAINER_ID."
+debug "Wait container $CONTAINER_ID."
 if [[ -n "$WAIT_TIME" ]]; then
-	echo "[wait-run]    method: sleep $WAIT_TIME seconds"
+	debug "   method: sleep $WAIT_TIME seconds"
 	__run
 	I=$WAIT_TIME
 	while [[ "$I" -gt 0 ]]; do
 		I=$(($I - 1))
 		if ! podman inspect --type=container "$CONTAINER_ID" &>/dev/null; then
-			echo "[wait-run] Failed wait container $CONTAINER_ID to stable." >&2
+			debug "Failed wait container $CONTAINER_ID to stable." >&2
 			sdnotify --status="gone"
 			exit 1
 		fi
-		echo "[wait-run] $I." >&2
+		debug "$I." >&2
 		sdnotify --status="wait:$I"
 		sleep 1
 	done
-	echo "[wait-run] Container still running."
+	debug "Container still running."
 elif [[ -n "$WAIT_OUTPUT" ]]; then
-	echo "[wait-run]    method: wait output '$WAIT_OUTPUT'"
+	debug "   method: wait output '$WAIT_OUTPUT'"
 	__run
 	(
 		IN=/tmp/$RANDOM.in.fifo
 		mkfifo $IN
 		podman attach --detach-keys=q --no-stdin=true --sig-proxy=false "$CONTAINER_ID" &>$IN &
 		PID=$!
-		echo "PID=$PID"
+		debug "PID=$PID"
 		while read line; do
 			if echo "$line" | grep -qE "$WAIT_OUTPUT"; then
-				echo "[wait-run] output found"
+				debug "output found"
 				kill -SIGKILL $PID
 				exit 0
 			fi
 		done <$IN
-		true
-	)
-	echo "[wait-run] got string"
+	) || die "Can not read output!"
+	debug "got string"
 elif [[ -n "$ACTIVE_FILE" ]]; then
-	if podman volume inspect ACTIVE_FILE 2>&1 | grep -q "no such volume" ; then
+	if podman volume inspect ACTIVE_FILE 2>&1 | grep -q "no such volume"; then
 		podman volume create ACTIVE_FILE
 	fi
 	ACTIVE_FILE_ROOT=$(podman volume inspect ACTIVE_FILE -f "{{.Mountpoint}}")
 	ACTIVE_FILE_ABS="$ACTIVE_FILE_ROOT/$ACTIVE_FILE"
 
-	echo "[wait-run]    method: wait file $ACTIVE_FILE_ABS to exists"
+	debug "   method: wait file $ACTIVE_FILE_ABS to exists"
 	sdnotify --status="wait:active"
 	rm -f "$ACTIVE_FILE_ABS"
 
@@ -68,10 +83,10 @@ elif [[ -n "$ACTIVE_FILE" ]]; then
 		sleep 1
 	done
 
-	echo "[wait-run] got file"
+	debug "got file"
 else
-	echo "[wait-run]    method: none"
+	debug "   method: none"
 fi
 
 sdnotify --ready --status="ok"
-echo "[wait-run] Conmon PID: $(<$PIDFile)"
+debug "Conmon PID: $(<$PIDFile)"
