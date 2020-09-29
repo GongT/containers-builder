4fd890d8d25704dc004338fac78c45c36737bc51
GongT <admin@gongt.me>
2020-09-29 21:00:33 +0800

    add run_apt

--- functions/alpine.sh
+++ functions/alpine.sh
@@ -4,6 +4,28 @@ function use_alpine_apk_cache() {
 	echo -e "--volume=$SYSTEM_COMMON_CACHE/apk:/etc/apk/cache"
 }
 
+function run_apt() {
+	local NAME=$1
+	shift
+
+	buildah run $(use_alpine_apk_cache) "$CONTAINER" sh -s- -- "${PKGS[@]}" <<- 'APK'
+		echo "APK: install $*"
+		echo "HTTP_PROXY=$HTTP_PROXY HTTPS_PROXY=$HTTPS_PROXY ALL_PROXY=$ALL_PROXY http_proxy=$http_proxy https_proxy=$https_proxy all_proxy=$all_proxy"
+		I=0
+		while [ "$I" -lt 3 ] ; do
+			if apk add "$@" ; then
+				exit 0
+			fi
+			apk update || true
+
+			I=$(($I + 1))
+			echo "failed... retry: $I"
+		done
+		echo "can not install package."
+		exit 1
+	APK
+}
+
 function make_base_image_by_apt() {
 	local BASEIMG=$1 NAME=$2
 	shift
@@ -16,7 +38,7 @@ function make_base_image_by_apt() {
 	_apk_build_cb() {
 		local CONTAINER
 		CONTAINER=$(new_container "$1" "$BASEIMG")
-		buildah run $(use_alpine_apk_cache) "$CONTAINER" apk add -U "${PKGS[@]}"
+		run_apt "$CONTAINER" "${PKGS[@]}"
 	}
 
 	if [[ "${FORCE_APK+found}" != found ]]; then
