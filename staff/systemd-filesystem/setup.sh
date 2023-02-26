3283b869021423327d2a453e127f5286006b2763
GongT <admin@gongt.me>
2023-02-26 18:57:49 +0800

    update systemd services

--- staff/systemd-filesystem/setup.sh
+++ staff/systemd-filesystem/setup.sh
@@ -2,5 +2,6 @@
 
 set -Eeuo pipefail
 
-systemctl disable rpmdb-migrate.service rpmdb-rebuild.service dnf-makecache.timer || true
+systemctl mask systemd-networkd-wait-online.service
+systemctl disable rpmdb-migrate.service rpmdb-rebuild.service dnf-makecache.timer systemd-logind.service systemd-oomd.service systemd-oomd.socket systemd-networkd.socket || true
 systemctl set-default multi-user.target
