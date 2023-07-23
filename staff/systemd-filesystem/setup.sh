e42975bfd2120eac4c66b024ac83adf210795a15
GongT <admin@gongt.me>
2023-07-23 21:13:00 +0800

    fix /etc/localtime

--- staff/systemd-filesystem/setup.sh
+++ staff/systemd-filesystem/setup.sh
@@ -5,3 +5,7 @@ set -Eeuo pipefail
 systemctl mask systemd-networkd-wait-online.service
 systemctl disable rpmdb-migrate.service rpmdb-rebuild.service dnf-makecache.timer systemd-logind.service systemd-oomd.service systemd-oomd.socket systemd-networkd.socket || true
 systemctl set-default multi-user.target
+
+if ! [[ -e /etc/localtime ]]; then
+	ln -s /usr/share/zoneinfo/Asia/Shanghai /etc/localtime
+fi
