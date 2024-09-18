608c2fbaca9920c8872dc269f08acd9522b9436f
GongT <admin@gongt.me>
2024-09-18 19:48:43 +0800

    fix wrong path

--- cli/actions/act_attach.sh
+++ cli/actions/act_attach.sh
@@ -5,18 +5,24 @@ function do_attach() {
 		die "missing arguments"
 	fi
 
-	TARGET="$1"
+	TARGET="$1" CMD=""
 	shift
 	if [[ $# -gt 0 ]]; then
 		CMD="$1"
 		shift
-	else
-		CMD="sh"
 	fi
 
 	local SERVICE_FULL_NAME=$(systemctl list-units '*.pod@.service' '*.pod.service' --no-pager --no-legend | grep "$TARGET" | grep -E '(running|activating)' | awk '{print $1}' | head -n1)
 	if [[ $SERVICE_FULL_NAME ]]; then
 		TARGET=$(get_container_by_service "$SERVICE_FULL_NAME")
+		if [[ -z ${CMD} ]]; then
+			ROOT=$(podman mount nginx || true)
+			if [[ -n ${ROOT} && -e "${ROOT}/usr/bin/bash" ]]; then
+				CMD='/usr/bin/bash'
+			else
+				CMD='/bin/sh'
+			fi
+		fi
 		echo -e " + podman exec -it \e[38;5;11m$TARGET\e[0m $CMD $*" >&2
 		exec podman exec -it "$TARGET" "$CMD" "$@"
 	else
