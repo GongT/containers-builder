e96bfe2e9c8701e140cda60fc49124ee352efd51
GongT <admin@gongt.me>
2025-01-29 00:50:25 +0800

    remove locale setting

--- staff/systemd-filesystem/basic/setup.sh
+++ staff/systemd-filesystem/basic/setup.sh
@@ -19,9 +19,4 @@ if [[ ! -e /etc/localtime ]]; then
 	ln -s /usr/share/zoneinfo/Asia/Shanghai /etc/localtime
 fi
 
-echo 'LANG=zh_CN.UTF-8
-LC_TIME=en_DK.UTF-8
-LC_MEASUREMENT=metric
-' >/etc/locale.conf
-
 rm -f /etc/machine-id
