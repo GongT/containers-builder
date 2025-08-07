5fac87b0dc76f8cfb411a453f8375f1cec700020
GongT <admin@gongt.me>
2025-08-07 12:14:18 +0800

    up

--- package/platform/users.sh
+++ package/platform/users.sh
@@ -1,23 +1,33 @@
 function ensure_user() {
 	local U_ID=$1 U_NAME=$2 G_ID_OR_NAME=$3
-	if user_exists "$U_NAME"; then
-		info_note "User $U_NAME exists with id $(get_uid_of_user "$U_NAME")"
+	if user_exists "${U_NAME}"; then
+		local EX_UID=$(get_uid_of_user "${U_NAME}")
+		if [[ ${EX_UID} -eq ${U_ID} ]]; then
+			info_note "User ${U_NAME} exists with id ${EX_UID}"
+		else
+			info_warn "User ${U_NAME} has a different id: exists=${EX_UID}, want=${U_ID}"
+		fi
 	else
 		local G_ID
-		G_ID=$(get_gid_of_group "$G_ID_OR_NAME")
+		G_ID=$(get_gid_of_group "${G_ID_OR_NAME}")
 
-		useradd --gid "$G_ID" --no-create-home --no-user-group --uid "$U_ID" "$U_NAME"
-		info_success "new system user $U_NAME ($(get_uid_of_user "$U_NAME"))"
+		useradd --gid "${G_ID}" --no-create-home --no-user-group --uid "$U_ID" "${U_NAME}"
+		info_success "new system user ${U_NAME} ($(get_uid_of_user "${U_NAME}"))"
 	fi
 }
 
 function ensure_group() {
 	local G_ID=$1 G_NAME=$2
-	if group_exists "$G_NAME"; then
-		info_note "Group $G_NAME exists with id $(get_gid_of_group "$G_NAME")"
+	if group_exists "${G_NAME}"; then
+		local EX_GID=$(get_gid_of_group "${G_NAME}")
+		if [[ ${EX_GID} -eq ${G_ID} ]]; then
+			info_note "Group ${G_NAME} exists with id ${EX_GID}"
+		else
+			info_warn "Group ${G_NAME} has a different id: exists=${EX_GID}, want=${G_ID}"
+		fi
 	else
-		groupadd -g "$G_ID" "$G_NAME"
-		info_success "new system group $G_NAME ($(get_gid_of_group "$G_NAME"))"
+		groupadd -g "${G_ID}" "${G_NAME}"
+		info_success "new system group ${G_NAME} ($(get_gid_of_group "${G_NAME}"))"
 	fi
 }
 
@@ -39,13 +49,13 @@ function group_exists() {
 
 function ensure_user_exists() {
 	local U_NAME=$1
-	if user_exists "$U_NAME"; then
-		info_note "User $U_NAME exists with id $(get_uid_of_user "$U_NAME")"
+	if user_exists "${U_NAME}"; then
+		info_note "User ${U_NAME} exists with id $(get_uid_of_user "${U_NAME}")"
 	else
 		local G_ID
 		G_ID=$(get_gid_of_group "users")
 
-		useradd --gid "$G_ID" --no-create-home --no-user-group "$U_NAME"
-		info_success "new system user $U_NAME ($(get_uid_of_user "$U_NAME"))"
+		useradd --gid "${G_ID}" --no-create-home --no-user-group "${U_NAME}"
+		info_success "new system user ${U_NAME} ($(get_uid_of_user "${U_NAME}"))"
 	fi
 }
--- staff/systemd-filesystem/basic/fs/entrypoint/library/users.sh
+++ staff/systemd-filesystem/basic/fs/entrypoint/library/users.sh
@@ -1,23 +1,33 @@
-function ensure_user() {
+function system_ensure_user() {
 	local U_ID=$1 U_NAME=$2 G_ID_OR_NAME=$3
-	if user_exists "$U_NAME"; then
-		log "User $U_NAME exists with id $(get_uid_of_user "$U_NAME")"
+	if user_exists "${U_NAME}"; then
+		local EX_UID=$(get_uid_of_user "${U_NAME}")
+		log "User ${U_NAME} exists with id ${EX_UID}"
+		if [[ ${EX_UID} -ne ${U_ID} ]]; then
+			log "User ${U_NAME} has a different id (${EX_UID} != ${U_ID}), updating..."
+			usermod -u "${U_ID}" "${U_NAME}"
+		fi
 	else
 		local G_ID
-		G_ID=$(get_gid_of_group "$G_ID_OR_NAME")
+		G_ID=$(get_gid_of_group "${G_ID_OR_NAME}")
 
-		useradd --gid "$G_ID" --no-create-home --no-user-group --uid "$U_ID" "$U_NAME"
-		log "new system user $U_NAME ($(get_uid_of_user "$U_NAME"))"
+		useradd --gid "${G_ID}" --no-create-home --no-user-group --uid "$U_ID" "${U_NAME}"
+		log "new system user ${U_NAME} ($(get_uid_of_user "${U_NAME}"))"
 	fi
 }
 
-function ensure_group() {
+function system_ensure_group() {
 	local G_ID=$1 G_NAME=$2
-	if group_exists "$G_NAME"; then
-		log "Group $G_NAME exists with id $(get_gid_of_group "$G_NAME")"
+	if group_exists "${G_NAME}"; then
+		local EX_GID=$(get_gid_of_group "${G_NAME}")
+		log "Group ${G_NAME} exists with id ${EX_GID}"
+		if [[ ${EX_GID} -ne ${G_ID} ]]; then
+			log "Group ${G_NAME} has a different id (${EX_GID} != ${G_ID}), updating..."
+			groupmod -g "${G_ID}" "${G_NAME}"
+		fi
 	else
-		groupadd -g "$G_ID" "$G_NAME"
-		log "new system group $G_NAME ($(get_gid_of_group "$G_NAME"))"
+		groupadd -g "${G_ID}" "${G_NAME}"
+		log "new system group ${G_NAME} ($(get_gid_of_group "${G_NAME}"))"
 	fi
 }
 
@@ -39,13 +49,13 @@ function group_exists() {
 
 function ensure_user_exists() {
 	local U_NAME=$1
-	if user_exists "$U_NAME"; then
-		log "User $U_NAME exists with id $(get_uid_of_user "$U_NAME")"
+	if user_exists "${U_NAME}"; then
+		log "User ${U_NAME} exists with id $(get_uid_of_user "${U_NAME}")"
 	else
 		local G_ID
 		G_ID=$(get_gid_of_group "users")
 
-		useradd --gid "$G_ID" --no-create-home --no-user-group "$U_NAME"
-		log "new system user $U_NAME ($(get_uid_of_user "$U_NAME"))"
+		useradd --gid "${G_ID}" --no-create-home --no-user-group "${U_NAME}"
+		log "new system user ${U_NAME} ($(get_uid_of_user "${U_NAME}"))"
 	fi
 }
